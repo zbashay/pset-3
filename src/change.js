12bc36d8b4e65b5fe97703c99b71f3c45685e630
Zaviay Bashay <ZBashay@ucvts.tec.nj.us>
2019-10-30 10:34:41 -0400

    change

--- src/change.js
+++ src/change.js
@@ -1,14 +1,26 @@
 const readlineSync = require("readline-sync");
 
 const MIN = 0.01;
-const MAX = 0.99;
+const MAX = 99;
+const QUARTERS = 25;
+const DIMES = 10;
+const NICKLES = 5;
+
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("\nInvalid")
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("\nInvalid")
 } else {
-    // write your code for making change here
+    let whole = amount * 100
+    let numOfQuarters = Math.floor(whole / QUARTERS);
+    let change1 = (whole % QUARTERS)
+    let numOfDimes = Math.floor(change1 / DIMES);
+    let change2 = (change1 % DIMES)
+    let numOfNickles = Math.floor(change2 / NICKLES);
+    let change3 = (change2 % NICKLES)
+
+    console.log( "\n" + numOfQuarters + " quarters, " + numOfDimes + " dimes, " + numOfNickles + " nickles, and " + change3 + " pennies.")
 }
