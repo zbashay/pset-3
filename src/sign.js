f50c000cfca2d40f3a255fb566d1b722b199fde9
Zaviay Bashay <ronatorino@gmail.com>
2019-11-01 10:00:59 -0400

    positive

---
+++ src/sign.js
@@ -0,0 +1,23 @@
+const readlineSync = require("readline-sync");
+
+const number = Number(readlineSync.question("\nEnter a number: "));
+
+const MIN = Number.MIN_SAFE_INTEGER
+const MAX = Number.MAX_SAFE_INTEGER
+
+if (Number.isNaN(number)) {
+    console.log("\nInvalid")
+  }
+   else if (number < MIN || number > MAX) {
+      console.log("\nInvalid")
+}
+
+  else if (number > 0){
+    console.log("\nPositive")
+}
+else if (number < 0){
+  console.log("\nNegative")
+}
+else {
+  console.log("\nZero")
+}
